8a138e9fa6d301ad6e2828ff4182dfcc5b0483dc
VNavrockij <vitalik.nav@gmail.com>
2023-08-31 13:43:21 +0300

    Initial Commit

---
+++ Natife-Trainee-Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Natife-Trainee-Test
+//
+//  Created by Vitalii Navrotskyi on 31.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
